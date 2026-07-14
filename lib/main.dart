5f9855d6cde11a7ab2321e36df24ed06e9c45855
JesusRodrigoV <jaicelvelasco105@gmail.com>
2026-07-13 22:47:11 -0400

    mejoras

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
-import 'config.dart';
 import 'providers.dart';
 
 void main() async {
